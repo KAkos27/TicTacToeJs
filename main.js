269347e37dfb46770bb452c332cccdb5a858fee6
KAkos27 <akoskosztolanyi@gmail.com>
2024-05-22 12:16:21 +0200

    forloop

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import Elem from "./Elem.js";
 
 const szuloElem = $(".jatekter");
-const elem = new Elem("X", szuloElem);
+for (let index = 0; index < 9; index++) {
+  const elem = new Elem("X", szuloElem);
+}
